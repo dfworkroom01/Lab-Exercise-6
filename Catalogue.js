d4642c3158dc30372ff6e00d4c639c1c58141abc
dfworkroom01 <dfworkroom01@gmail.com>
2024-07-13 18:02:19 +0800

    JSON data from array

---
+++ Catalogue.js
@@ -0,0 +1,23 @@
+const catalogue1= {
+    "Title" :"TITLE: The Sign of the Four",
+    "Description":"DESCRIPTION: Mistery",
+    "Author":"AUTHOR: Arthur Conan Doyle",
+    "Pages": "TOTAL PAGES: 117"
+
+}
+console.log(catalogue1.Title)
+console.log(catalogue1.Description)
+console.log(catalogue1.Author)
+console.log(catalogue1.Pages)
+
+
+const catalogue2= { 
+    "Title" :"TITLE: The Attic Murder",
+    "Description":"DESCRIPTION: Thriller",
+    "Author":"AUTHOR: Fowler Right",
+    "Pages": "TOTAL PAGES: 212"
+    }
+console.log(catalogue2.Title)
+console.log(catalogue2.Description)
+console.log(catalogue2.Author)
+console.log(catalogue2.Pages)
