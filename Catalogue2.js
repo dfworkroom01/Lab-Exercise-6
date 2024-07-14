e90d24e4f8decda823cebded8dc8158c75b270fe
dfworkroom01 <dfworkroom01@gmail.com>
2024-07-14 18:46:23 +0800

    Revise keys and check keys

--- Catalogue2.js
+++ Catalogue2.js
@@ -1,15 +1,15 @@
 let data1 = {
-    "Title" :"The Sign of the Four",
-    "Description":"Mistery",
-    "Author":"Arthur Conan Doyle",
-    "Pages": "117"
+    Title:'The Sign of the Four',
+    Description:'Mistery',
+    Author:'Arthur Conan Doyle',
+    Pages:117
     }
 
 let data2 = {
-    "Title" :"The Attic Murder",
-    "Description":"Thriller",
-    "Author":"Fowler Right",
-    "Pages": "212"
+    Title:'The Attic Murder',
+    Description:'Thriller',
+    Author:'Fowler Right',
+    Pages:212
     }
 
 let alldata1 =  new Array()
@@ -21,25 +21,25 @@ console.log(alldata1)
 //extension
 
 let data3 = {
-    "Title" :"The Phantom of the Opera",
-    "Description":"Psychological",
-    "Author":"Gaston Leroux",
-    "Pages": "254"
+    Title:'The Phantom of the Opera',
+    Description:'Psychological',
+    Author:'Gaston Leroux',
+    Pages:254
     }
 
 let data4 = {
-    "Title" :"Criminal Psychology",
-    "Description":"Psychological",
-    "Author":"Hans Gross",
-    "Pages": "740"
+    Title:'Criminal Psychology',
+    Description:'Psychological',
+    Author:'Hans Gross',
+    Pages:740
     }
 
 let data5 = {
-        "Title" :"The Thirty-Nine Steps",
-        "Description":"History",
-        "Author":"John Buchan",
-        "Pages": "101"
-        }
+    Title:'The Thirty-Nine Steps',
+    Description:'History',
+    Author:'John Buchan',
+    Pages:101
+    }
 
 let alldata =  new Array()
     alldata.push(data1)
@@ -49,3 +49,9 @@ let alldata =  new Array()
     alldata.push(data5)
      
 console.log(alldata) 
+
+if (Object.keys(data1, data2, data3, data4, data5).includes('Title', 'Description', 'Author', 'Pages')) {
+    console.log('true');
+} else {
+    console.log('false');
+}
